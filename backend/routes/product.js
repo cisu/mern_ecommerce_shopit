5a632fe2f84114272ce81697c9b39403deb19978
cisu <49948070+cisu@users.noreply.github.com>
2021-01-15 18:57:49 +0200

    Admin - Delete Product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,7 +5,8 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
-  updateProduct
+  updateProduct,
+  deleteProduct
 } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
@@ -13,6 +14,10 @@ router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(newProduct);
 
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id')
+                .put(updateProduct)
+                .delete(deleteProduct);
+
+
 
 module.exports = router;
