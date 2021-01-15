00e34e98de37d77cb85239897fabc1b4b108e916
cisu <49948070+cisu@users.noreply.github.com>
2021-01-15 23:46:53 +0200

    show erros in prod and dev mode

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -4,11 +4,28 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) =>{
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || 'Internal Server Error';
+    
+    // Show the errors in development mode
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        })
+    }
 
-    res.status(err.statusCode).json({
-        success: false,
-        error: err.stack
-    })
+    // Show the errors in production mode
+    if (process.env.NODE_ENV === 'PRODUCTION'){
+        let error = {...err}
+
+        error.message = err.message
+
+        res.status(err.statusCode).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        })
+    }
+
 }
 
