f6e7d5ab2fa713eab5128a3f3aeb38d99c92ec3f
cisu <49948070+cisu@users.noreply.github.com>
2021-01-16 13:10:38 +0200

    Handling Uncaught Exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,16 @@ const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
+// Handle Uncaught exceptions
+process.on('uncaughtException', err => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log('Shutting down server due to uncaught exception');
+  process.exit(1);
+});
+
 // Setting up config file
 dotenv.config({path: 'backend/config/config.env'});
+
 
 // Connecting to database
 connectDatabase();
