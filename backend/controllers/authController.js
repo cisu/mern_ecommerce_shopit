0472b67d16afafb7d1de11e2c356b1bed8302676
cisu <49948070+cisu@users.noreply.github.com>
2021-01-17 21:16:47 +0200

    Sending JWT Token in Cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // Register a user     => /api/v1/registers
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -17,12 +18,8 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken();
+  sendToken(user, 200, res);
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
 });
 
 // Login User   =>    /api/v1/Login
@@ -49,10 +46,6 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler('Invalid email or password', 401));
   }
 
-  const token = user.getJwtToken();
 
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
 });
