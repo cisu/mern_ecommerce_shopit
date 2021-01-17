a711263d4df8c18611a6bbaf49f7a42365e4cc93
cisu <49948070+cisu@users.noreply.github.com>
2021-01-17 22:21:21 +0200

    Protect Routes from Unauthorized Users

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,18 +6,16 @@ const {
   newProduct,
   getSingleProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 } = require('../controllers/productController');
 
-router.route('/products').get(getProducts);
+const {isAuthenticatedUser} = require('../middlewares/auth');
+
+router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(newProduct);
 
-router.route('/admin/product/:id')
-                .put(updateProduct)
-                .delete(deleteProduct);
-
-
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
