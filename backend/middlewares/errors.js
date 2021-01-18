28e71b1a614bea4c4b858364eb24663dc97d5149
cisu <49948070+cisu@users.noreply.github.com>
2021-01-18 18:16:24 +0200

    Handle Wrong JWTToken & Expire JWT Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -28,7 +28,25 @@ module.exports = (err, req, res, next) => {
     // Handling Mongoose validation Error
     if (err.name === 'validationError') {
       const message = Object.values(err.errors).map(value => value.message);
-      error = newErrorHandler(message, 400);
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose duplicate key errors
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling wrong JWT error
+    if (err.name === 'JsonExpiredError') {
+      const message = 'JSON Web Token is expired. Try Again!!!';
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Expired JWT error
+    if (err.name === 'JsonWebTokenError') {
+      const message = 'JSON Web Token is invalid. Try Again!!!';
+      error = new ErrorHandler(message, 400);
     }
 
     res.status(err.statusCode).json({
