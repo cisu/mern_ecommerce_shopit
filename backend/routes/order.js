1d8bc64211583fb662cb3d2f8d5f60c001665b60
cisu <49948070+cisu@users.noreply.github.com>
2021-01-20 16:12:34 +0200

    Create & save order fail

---
+++ backend/routes/order.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router();
+
+const {
+    newOrder,
+    getSingleOrder,
+    myOrders,
+    allOrders,
+    updateOrder,
+    deleteOrder
+
+} = require('../controllers/orderController')
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
+
+router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+module.exports = router;
